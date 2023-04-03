a844880523e4241ed5786a240b0b032de39bdfb5
Zelanious <71904533+Zelanious@users.noreply.github.com>
2023-04-03 17:21:20 +0100

    Faun Growth Ability draft

---
+++ Source/Roos Faun Xenotype/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Roos_Faun_Xenotype;
+using Verse;
+
+namespace Roos_Faun_Xenotype
+{
+    [StaticConstructorOnStartup]
+    public static class RBSF_Faun
+    {
+        static RBSF_Faun()
+        {
+            Log.Message("FAUN STATIC CONSTRUCTOR lOADED.");
+        }
+    }
+}
